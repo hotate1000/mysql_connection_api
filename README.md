fb41ac82a021b0717fb2d0303c3433f3da29f7bd
hotate1000 <ooo.336.sss@gmail.com>
2023-05-08 21:20:07 +0900

    mysql接続テスト

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+## 必要パッケージ
+
+- pip install Flask：Flaskを利用する。
+- pip install python-dotenv：.envファイルを利用する。
+- pip install pymysql：mysqlを利用する。
+- pip install flake8：文字整形。
--- apps/mysql_connection/views.py
+++ apps/mysql_connection/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template;
+import pymysql.cursors;
+import os;
 
 
 # Blueprint: アプリを分割するためのFlaskの機能
@@ -11,6 +13,37 @@ mysql_connection = Blueprint(
 )
 
 
+# mysqlへ接続する
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    # mysqlのパスワードを入力する。
+    password=os.environ.get("MYSQL_PASSWORD"),
+    # 操作するデータベース名を入力する。
+    db=os.environ.get("MYSQL_DB_NAME"),
+    # データを辞書型で渡す。
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+
 @mysql_connection.route("/")
 def index():
     return "Hello World";
+
+
+@mysql_connection.route("select")
+def select():
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM user;"
+            # sqlの実行。
+            cursor.execute(sql);
+            # 実行したsqlの表示。
+            print("\n---実行したsql文---\n", cursor._executed);
+            # テーブルデータを取得する。
+            db_data = cursor.fetchall();
+            print(db_data);
+    finally:
+        connection.close();
+    if db_data:
+        return db_data;    
